d86b9495e60f770d14b23a109c2acf6932142921
Aldo Cedillo <a.cedillo@hlcn.mx>
2026-09-12 16:11:40 -0600

    Renaming first migration

--- db/migrations/001_create_butik_schemas.sql
+++ db/migrations/001_create_butik_schemas.sql
@@ -1,5 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
-CREATE EXTENSION IF NOT EXISTS postgis;
+-- TODO: enable postgis extension if needed
+-- CREATE EXTENSION IF NOT EXISTS postgis;
 
 CREATE SCHEMA IF NOT EXISTS butiks_engine;
 -- CREATE SCHEMA IF NOT EXISTS unio_tickets_engine;
